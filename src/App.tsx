4a80c1ef8aaffd6aa84a82d9be2edf902becd221
tunahan <ttezcan.1999@gmail.com>
2025-05-24 15:12:00 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Box from './components/box'
 export default function App() {
     return (
         <div className='bg-red-300'>
-            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
             <Box />
         </div>
     )
