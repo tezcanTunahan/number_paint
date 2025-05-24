f8d56b807882775039f529d1b87855071e92ea42
tunahan <ttezcan.1999@gmail.com>
2025-05-24 15:11:56 +0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Box from './components/box'
+
+export default function App() {
+    return (
+        <div className='bg-red-300'>
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+            <Box />
+        </div>
+    )
+}
